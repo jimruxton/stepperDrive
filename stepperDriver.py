84a9ba3e1763d3c91bb1b07adc298e993137de74
Jim Ruxton <jim.ruxton@gmail.com>
2018-06-15 05:48:03 +0000

    fastest version using sleep

--- stepperDriver.py
+++ stepperDriver.py
@@ -3,9 +3,10 @@ import RPi.GPIO as GPIO
 
 DIR = 20   # Direction GPIO Pin
 STEP = 21  # Step GPIO Pin
-CW = 1     # Clockwise Rotation
-CCW = 0    # Counterclockwise Rotation
-SPR = 5373   # Steps per Revolution (360 / .067)
+CW = 0    # Clockwise Rotation
+CCW = 1    # Counterclockwise Rotation
+SPR = 5373*2   # Steps per Revolution (360 / .067)
+cycles=1000
 
 GPIO.setmode(GPIO.BCM)
 #GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -15,20 +16,22 @@ GPIO.setup(STEP, GPIO.OUT)
 GPIO.output(DIR, CW)
 
 step_count = SPR
-delay = .0208
+delay = .0000001
+for y in range(cycles):
+    GPIO.output(DIR, CW)
+    for x in range(step_count):
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay)
 
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
-
-sleep(.5)
-GPIO.output(DIR, CCW)
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
+    sleep(.3)
+    GPIO.output(DIR, CCW)
+    for x in range(step_count):
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay)
+    sleep(.3)
 
 GPIO.cleanup()
